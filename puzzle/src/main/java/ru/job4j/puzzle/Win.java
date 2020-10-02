06841c19ffc0705f8962e061edab47954d751e79
Давлат <davlat51@gmail.com>
2020-10-03 00:21:00 +0300

    Игра Сокобан

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,38 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
+        for (int row = 0; row < board.length; row++)
+            if (board[row][row] == 1) {
+                if(monoHorizontal(board, row) || monoVertical(board, row)){
+                    rsl = true;
+                    break;
+                }
+            }
         return rsl;
     }
+
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean rsl = true;
+        for (int cell = 0; cell < board.length; cell++) {
+            if (board[row][cell] != 1){
+                rsl = false;
+                break;
+            }
+        }
+         return rsl;
+    }
+
+    public static boolean monoVertical(int[][] board, int cell) {
+        boolean rsl = true;
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][cell] != 1){
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
 }
+
